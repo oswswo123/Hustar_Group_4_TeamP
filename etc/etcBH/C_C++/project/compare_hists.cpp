58b6f011f8b8b852cf47b7cfd8dee1e0a1e21ac1
bye-hwa <cyo2625@naver.com>
2022-04-19 14:19:55 +0900

    update compare_face file

---
+++ etc/etcBH/C_C++/project/compare_hists.cpp
@@ -0,0 +1,85 @@
+#include <opencv2/opencv.hpp>
+#include "preprocess.hpp"
+#include "correct_angle.hpp"
+#include "detect_area.hpp"
+#include "histo.hpp"
+
+cv::Point2d calc_center(cv::Rect obj)
+{
+    cv::Point2d c = (cv::Point2d)obj.size() / 2.0;
+    cv::Point2d center = (cv::Point2d)obj.tl() + c;
+    return center;
+}
+
+int main()
+{
+    cv::CascadeClassifier face_cascade, eyes_cascade;
+    load_cascade(face_cascade, "haarcascade_frontalface_alt2.xml");
+    load_cascade(eyes_cascade, "haarcascade_eye.xml");
+
+    cv::Mat image = cv::imread("photo.jpg", cv::IMREAD_COLOR);
+    CV_Assert(image.data);
+    cv::Mat gray = preprocessing(image);
+
+    std::vector<cv::Rect>  faces, eyes;
+    std::vector<cv::Point2d> eyes_center;
+    face_cascade.detectMultiScale(gray, faces, 1.1, 2, 0, cv::Size(100, 100));
+    std::vector<cv::Rect>  sub_obj;
+
+
+    if (faces.size()>0)
+    {
+        float i = 1.15;
+        while(true)
+        {
+            eyes_cascade.detectMultiScale(gray(faces[0]),eyes,i,7,0,cv::Size(25,20));
+            if (eyes.size() == 2)
+            {
+                eyes_center.push_back(calc_center(eyes[0]+faces[0].tl()));
+                eyes_center.push_back(calc_center(eyes[1]+faces[0].tl()));
+
+                cv::Point2d face_center = calc_center(faces[0]);
+                cv::Mat rot_mat = calc_rotMap(face_center, eyes_center);
+                cv::Mat correct_img = correct_image(image, rot_mat, eyes_center);
+
+                cv::circle(correct_img, eyes_center[0], 5, cv::Scalar(0,255,0),2);
+                cv::circle(correct_img, eyes_center[1], 5, cv::Scalar(0,255,0),2);
+ 
+                detect_hair(face_center, faces[0], sub_obj);
+                // sub_obj.push_back(detect_lip(face_center, faces[0]));
+                sub_obj.push_back(detect_forehead(face_center, faces[0]));
+                sub_obj.push_back(detect_brow(eyes_center[0],eyes_center[1], faces[0]));
+
+                cv::Mat masks[4], hists[4];
+                make_masks(sub_obj, correct_img.size(), masks);
+                calc_histos(correct_img, sub_obj, hists, masks);  
+
+                double criteria1 = cv::compareHist(hists[0], hists[1], CV_COMP_CORREL);
+                double criteria2 = cv::compareHist(hists[2], hists[3], CV_COMP_CORREL);
+                //double criteria3 = cv::compareHist(hists[4], hists[5], CV_COMP_CORREL);
+                std::cout << cv::format("머리부분 유사도 %4.2f\n",criteria1);
+                std::cout << cv::format("눈썹부분 유사도 %4.2f\n",criteria2);
+                //std::cout << cv::format("눈썹부분 유사도 %4.2f\n",criteria3);
+
+
+
+                cv::imshow("sub_obj[0]", correct_img(sub_obj[0]));
+                cv::imshow("sub_obj[1]", correct_img(sub_obj[1]));
+                cv::imshow("sub_obj[2]", correct_img(sub_obj[2]));
+                cv::imshow("sub_obj[3]", correct_img(sub_obj[3]));
+
+                cv::rectangle(correct_img, sub_obj[2], cv::Scalar(255,0,0),2);
+                cv::rectangle(correct_img, sub_obj[3], cv::Scalar(255,0,0),2);
+
+                cv::imshow("correct_img", correct_img);
+                cv::waitKey();
+                break;
+            }
+             i += 0.05;
+            if (i > 10) break;
+
+        }
+
+    }
+    return 0;
+}
--- etc/etcBH/C_C++/project/histo.hpp
+++ etc/etcBH/C_C++/project/histo.hpp
@@ -14,4 +14,37 @@ void calc_Histo(const cv::Mat& img, cv::Mat& hist, cv::Vec3i bins, cv::Vec3f ran
     cv::calcHist(&img, 1, channels, mask, hist, dims, histSize, ranges);
 }
 
-void make_masks()
+void draw_ellipse(cv::Mat& image, cv::Rect2d obj, cv::Scalar color, int thickness, float ratio)
+{
+    cv::Point2d center = obj.tl() + (cv::Point2d)obj.size() * 0.5;
+    cv::Size2d size = (cv::Size2d)obj.size() * 0.45;
+    cv::ellipse(image, center, size, 0, 0, 360, color, thickness);
+}
+
+void make_masks(std::vector<cv::Rect> sub_obj, cv::Size org_size, cv::Mat mask[4]) 
+{
+    cv::Mat base_mask(org_size, CV_8U, cv::Scalar(0));
+    draw_ellipse(base_mask, sub_obj[2], cv::Scalar(255), -1, 0.45f);
+
+    mask[0] = base_mask(sub_obj[0]);
+    mask[1] = base_mask(sub_obj[1]);
+
+    draw_ellipse(base_mask, sub_obj[3], cv::Scalar(255), -1, 0.45f);
+    mask[3] = base_mask(sub_obj[3]);
+    mask[2] = base_mask(sub_obj[2]);
+}
+
+
+void calc_histos(cv::Mat correct_img, std::vector<cv::Rect> sub_obj, cv::Mat hists[4], cv::Mat masks[4])
+{
+    cv::Vec3i bins(64, 64, 64);
+    cv::Vec3i ranges(256, 256, 256);
+
+    for (int i = 0; i < (int)sub_obj.size(); i++)
+    {
+        cv::Mat sub = correct_img(sub_obj[i]);
+        calc_Histo(sub, hists[i], bins, ranges, masks[i]);
+        cv::imshow(cv::format("mask[%d]",i), masks[i]);
+        // cv::waitKey();
+    }
+}
