ca0ab13a43fea0e96f278f9025710d28ac7189d1
SeungHwan Oh <oswswo123@gmail.com>
2022-04-20 15:09:57 +0900

    SH) 4.20 image_processing project A_diro update

--- testSeungHwan/image_processing/A_diro_test/main.cpp
+++ testSeungHwan/image_processing/A_diro_test/main.cpp
@@ -1,32 +1,78 @@
 #include <opencv2/opencv.hpp>
-
-using namespace cv;
-using namespace std;
+#include "image_filter.h"
+#include "set_roi.h"
+#include "hough_cal.h"
+#include "hough_regression.h"
 
 int main() {
-	Mat image = imread("./road1.jpg", 1);
-	imshow("원본", image);
+	// 이미지 실행
+	//Mat image = imread("./road1.jpg", 1);
+	//imshow("원본", image);
 
-	Mat img_filter;
-	cvtColor(image, img_filter, COLOR_BGR2GRAY);
-	imshow("grayscale", img_filter);
+	// 동영상 실행(while 실행)
+	cv::Mat image;
+	cv::VideoCapture cap("./input.mp4");
+	cap.read(image);
 
-	Mat img_edges;
-	Canny(img_filter, img_edges, 50, 150);
-	imshow("Canny", img_edges);
+	//VideoWriter cap_writer, cap_writer_hough;
+	int codec = cv::VideoWriter::fourcc('X', 'V', 'I', 'D');
+	double fps = 20.0;
+	//cap_writer.open("./output.avi", codec, fps, image.size(), CV_8UC3);
+	//cap_writer_hough.open("./output_hough.avi", codec, fps, image.size(), CV_8UC3);
+
 	
-	vector<Vec4i> lines;
-	HoughLinesP(img_edges, lines, 1, CV_PI / 180, 50, 50, 5);
+	while (true) {
+		cap >> image;
 
-	Mat img_hough;
-	image.copyTo(img_hough);
-	for (size_t i = 0; i < lines.size(); i++) {
-		Vec4i I = lines[i];
-		line(img_hough, Point(I[0], I[1]), Point(I[2], I[3]), Scalar(0, 0, 255), 2, 8);
+		// 색상 필터 영역
+		cv::Mat img_filter;
+		img_filter = color_filter(image, false);
+		cv::imshow("filtering", img_filter);
+
+		// Gray Scale
+		cv::cvtColor(img_filter, img_filter, cv::COLOR_BGR2GRAY);
+		//imshow("grayscale", img_filter);
+
+		// Canny Edge 검출
+		cv::Mat img_edges;
+		cv::Canny(img_filter, img_edges, 50, 150);
+		//imshow("Canny", img_edges);
+
+		// !! ROI 지정 영역 !!
+		cv::Mat img_roi;
+		img_roi = setting_roi(img_edges);
+
+		// !! 허프 연산 영역 !!
+		std::vector<cv::Vec4i> lines;
+		lines = hough_transform(img_roi, 30, 20, 10, false);
+
+		// !! Linear Regression 영역 !!
+		std::vector<cv::Point> detection_lanepoint(4);
+		detection_lanepoint = regression_and_detection_points(img_roi, lines);
+
+		// !! line draw 영역 !!
+		// 차선 사이 색칠
+		std::vector<cv::Point> draw_points;
+		cv::Mat img_draw;
+		image.copyTo(img_draw);
+
+		draw_points.push_back(detection_lanepoint[0]);
+		draw_points.push_back(detection_lanepoint[1]);
+		draw_points.push_back(detection_lanepoint[3]);
+		draw_points.push_back(detection_lanepoint[2]);
+
+		cv::fillConvexPoly(img_draw, draw_points, cv::Scalar(0, 230, 0), cv::LINE_AA, 0);
+		cv::addWeighted(img_draw, 0.3, image, 0.7, 0, image);
+		// 차선 그리기
+		cv::line(image, detection_lanepoint[0], detection_lanepoint[1], cv::Scalar(0, 255, 0), 3, cv::LINE_AA);
+		cv::line(image, detection_lanepoint[2], detection_lanepoint[3], cv::Scalar(0, 255, 0), 3, cv::LINE_AA);
+		// !! line draw 영역 !!
+
+		//cap_writer << image;
+
+		cv::imshow("처리 후 결과", image);
+		if (cv::waitKey(10) == 27) break;
 	}
-	imshow("hough", img_hough);
-	
-
-	waitKey(0);
+	//cv::waitKey(0);
 	return 0;
 }
