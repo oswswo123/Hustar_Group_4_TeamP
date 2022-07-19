0968fede32441c1c05359a5ed2724f731ff7e97e
minhub7 <alsgh1125@gmail.com>
2022-07-19 15:47:38 +0900

    delete Secret key

--- testBN/Hustarproject/mysite/config/settings.py
+++ testBN/Hustarproject/mysite/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d)1ce^(p=$nl04gf%jzmc6jckfs&ka#6ev4l6!*nrh0!yjww73'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
