b1cfe75d41c7ee9141474cf9af5a68dbbd32e02f
minhub7 <alsgh1125@gmail.com>
2022-08-11 14:48:27 +0900

    MH) Update Graph & views.py

--- testMH/Ainalyst/apps/home/views.py
+++ testMH/Ainalyst/apps/home/views.py
@@ -13,53 +13,57 @@ from .models import Report
 from datetime import timedelta, date
 from collections import Counter
 
-START_DATE = date.today() - timedelta(days=30)
-END_DATE = date.today()
-
 
 @login_required(login_url="/login/")
 def index(request):
+    pos_Q, neg_Q = Q(new_opinion='매수'), Q(new_opinion='매도')    # 매수 / 매도 report 선택을 위한 QuerySet
+    start, end = date.today() - timedelta(days=30), date.today()    # 한달간 데이터 선택을 위한 Variable
+
     report_list = Report.objects.order_by('-create_date', '-id')
-    new_report = report_list.filter(create_date='2022-07-28')
-    pos_Q = Q(opinion='Buy') | Q(opinion='StrongBuy') | Q(opinion='매수') | Q(opinion='강력매수')
+    new_report = report_list.filter(create_date=date.today())
     positive_reports = report_list.filter(pos_Q)
-    negative_reports = report_list.filter(~pos_Q)
-    new_positive = positive_reports.filter(create_date__range=[START_DATE, END_DATE])
-    new_negative = negative_reports.filter(create_date__range=[START_DATE, END_DATE])
-    hot_topic = Counter([report.company for report in new_report]).most_common()
+    negative_reports = report_list.filter(neg_Q)
+    new_positive = positive_reports.filter(create_date__range=[start, end])
+    new_negative = negative_reports.filter(create_date__range=[start, end])
+    hot_topic = Counter([report.company for report in report_list.filter(create_date__range=[start, end])]).most_common()
 
     # Variable for graph
     # 1년치 데이터 선택, range: 2021-08.01 ~ 2022-08-09
     start, end = (date.today() - timedelta(days=365)).replace(day=1), date.today()
-    last_year_pos_reports = Report.objects.filter(Q(create_date__range=[start, end]) & pos_Q).values('create_date')\
-                                          .annotate(counts=Count('create_date'))
-    last_year_neg_reports = Report.objects.filter(Q(create_date__range=[start, end]) & (~pos_Q)).values('create_date')\
-                                          .annotate(counts=Count('create_date'))
 
-    #  [{create_date: 날짜, counts: 해당 날짜에 올라온 리포트 수 }]
-    counts = set(map(lambda day: day['create_date'].strftime("%Y-%m"), last_year_pos_reports))
-    months = sorted(list(counts))
-    dict_counts = {'positive': {count: 0 for count in counts}, 'negative': {count: 0 for count in counts}}
+    #  결과: [{create_date: 날짜, counts: 해당 날짜에 올라온 리포트 수 }]
+    recent_positive = Report.objects.filter(Q(create_date__range=[start, end]) & pos_Q)\
+                                    .values('create_date')\
+                                    .annotate(counts=Count('create_date'))
+    recent_negative = Report.objects.filter(Q(create_date__range=[start, end]) & neg_Q)\
+                                    .values('create_date')\
+                                    .annotate(counts=Count('create_date'))
 
-    for report in last_year_pos_reports:
-        dict_counts['positive'][report['create_date'].strftime("%Y-%m")] += report['counts']
-    for report in last_year_neg_reports:
-        dict_counts['negative'][report['create_date'].strftime("%Y-%m")] += report['counts']
+    # years: 2022-07, 2022-06, 2022-05, ... 2021-08로 나눠진 월별 데이터를 정렬된 형태로 저장
+    years = sorted(list(set(map(lambda day: day['create_date'].strftime("%Y-%m"), recent_positive))))
+    dict_years = {'positive': {month: 0 for month in years}, 'negative': {month: 0 for month in years}}
 
-    pos_counts = [count for _, count in sorted(dict_counts['positive'].items())]
-    neg_counts = [count for _, count in sorted(dict_counts['negative'].items())]
+    # 작성 날짜에 해당하는 달에 누적합 => 월별 리포트 수가 나옴
+    for report in recent_positive:
+        dict_years['positive'][report['create_date'].strftime("%Y-%m")] += report['counts']
+    for report in recent_negative:
+        dict_years['negative'][report['create_date'].strftime("%Y-%m")] += report['counts']
+
+    # 월별 긍정 리포트 수, 부정 리포트 수만 list에 저장 - graph 출력용
+    pos_counts = [count for _, count in sorted(dict_years['positive'].items())]
+    neg_counts = [count for _, count in sorted(dict_years['negative'].items())]
 
     context = {'segment': 'index',
-               'total_num_report': len(report_list),
-               'total_num_positive': len(positive_reports),
-               'total_num_negative': len(negative_reports),
-               'new_reports': len(new_report),
-               'new_positive': len(new_positive),
-               'new_negative': len(new_negative),
-               'hot_topic': hot_topic[:3],
-               'months': months,
-               'pos_counts': pos_counts,
-               'neg_counts': neg_counts,
+               'total_num_report': len(report_list),    # DB에 저장된 전체 리포트 수
+               'total_num_positive': len(positive_reports),    # 전체 긍정 리포트 수
+               'total_num_negative': len(negative_reports),    # 전체 부정 리포트 수
+               'new_reports': len(new_report),    # 당일 올라온 새 리포트 수
+               'new_positive': len(new_positive),    # 최근 한달 긍정 리포트 수
+               'new_negative': len(new_negative),    # 최근 한달 부정 리포트 수
+               'hot_topic': hot_topic[:3],    # 오늘 올라온 리포트 중 수가 가장 많은 것 Top 3
+               'years': years,    # Graph에 나타낼 최근 1년의 기간을 월별로 나눈 것
+               'pos_counts': pos_counts,    # 최근 1년간 월별 긍정 리포트 수
+               'neg_counts': neg_counts,    # 최근 1년간 월별 부정 리포트 수
                }
     html_template = loader.get_template('home/index.html')
     return HttpResponse(html_template.render(context, request))
@@ -71,7 +75,6 @@ def tables(request):
     page = request.GET.get('page', '1')
     paginator = Paginator(report_list, 10)
     page_obj = paginator.get_page(page)
-
     context = {'segment': 'index',
                'reports': page_obj,
                }
@@ -84,7 +87,6 @@ def tables_detail(request, report_id):
     context = {'segment': 'index',
                'report': report,
                }
-
     html_template = loader.get_template('home/tables_detail.html')
     return HttpResponse(html_template.render(context, request))
 
@@ -117,14 +119,3 @@ def pages(request):
     except:
         html_template = loader.get_template('home/page-500.html')
         return HttpResponse(html_template.render(context, request))
-
-
-# # printout Report list
-# def report(request):
-#     reportdb = PostgresDB()
-#     report_list = reportdb.execute(f"SELECT * FROM home_report ORDER BY create_date DESC;")
-#     print(report_list)
-#     # report_list = Report.objects.order_by('-create_date')
-#     context = {'report_list': report_list}
-#     html_template = loader.get_template('home/report_list.html')
-#     return HttpResponse(html_template.render(context, request))
--- testMH/test_information.py
+++ testMH/test_information.py
@@ -10,31 +10,32 @@ def main():
         print(f"Failed connect to db: ERROR code ({e})")
         return -1
 
-    ainalyst.execute(f"DELETE FROM home_report;")
+    # ainalyst.execute(f"DELETE FROM home_report;")
     # datas = ainalyst.execute("SELECT * FROM home_report;")
-    # print("SELECT * FROM home_report;")
-    # print("-"*100)
+    # print("SELECT * FROM home_report;", end='\n')
     # for d in datas:
     #     print(d)
     # print("-"*100)
     # print()
 
-    df = pd.read_csv('./report_dataset.csv')
+    df = pd.read_csv('./convert_inference_data.csv')
 
-    for idx, (_, company, title, article, opinion, firm, date) in df.iterrows():
-        print(f"{idx}: {company}, {title}, {opinion}, {firm}, {date}")
-        ainalyst.execute(f"INSERT INTO home_report(create_date, company, title, article, opinion, new_opinion, firm)"
-                         f" VALUES(%s, %s, %s, %s, %s, %s, %s);", (date, company, title, article, opinion, None, firm))
+    for idx, (company, title, article, opinion, firm, date, predictions, pred_rate) in df.iterrows():
+        print(f"{idx}: {date}, {company}, {title}, {opinion}, {predictions}, {pred_rate}")
+        # ainalyst.execute(f"INSERT INTO home_report(create_date, company, title, article, opinion, new_opinion, firm)"
+        #                  f" VALUES(%s, %s, %s, %s, %s, %s, %s);", (date, company, title, article, opinion, predictions, firm))
+        ainalyst.execute(f"UPDATE home_report SET pred_rate='%s'"
+                         f"WHERE create_date=%s AND company=%s AND title=%s AND article=%s AND opinion=%s AND new_opinion=%s AND firm=%s",
+                         (pred_rate, date, company, title, article, opinion, predictions, firm))
 
     print("Done")
-
     # print()
+
     # datas = ainalyst.execute("SELECT * FROM home_report;")
-    # print("SELECT * FROM home_report;")
-    # print("-" * 100)
+    # print("SELECT * FROM home_report;", end='\n')
     # for d in datas:
     #     print(d)
-    # print("-" * 100)
+
 
 if __name__ == '__main__':
     main()
