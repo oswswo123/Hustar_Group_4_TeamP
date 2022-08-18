52d77520cbac9ab675b3e2932fb20bfcd7a1882f
minhub7 <alsgh1125@gmail.com>
2022-08-18 12:04:26 +0900

    MH) Update KoBertForAinalyst file (batchs)

--- testMH/Ainalyst/core/settings.py
+++ testMH/Ainalyst/core/settings.py
@@ -18,8 +18,8 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'), '43.200.47.206']
-
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'), '43.200.47.206']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'), '15.164.60.79', config('SERVER', default='15.164.60.79')]
 # Application definition
 
 INSTALLED_APPS = [
